9c2eb3866ef2cbed881c72fa4de3b11525a4bd0b
Paweł Gąsiorowski <pgasiorowski@bluesoft.com>
2026-07-30 21:22:53 +0200

    extended issuer interface

--- artifact.go
+++ artifact.go
@@ -112,6 +112,7 @@ type ValidationSchemer[A Artifacter, O any] interface {
 // IssueSchemer issues an artifact of type A using scheme-specific options O.
 type IssueSchemer[O any, A Artifacter] interface {
 	Issue(ctx context.Context, options ...O) (A, error)
+	IssueForPrincipal(ctx context.Context, principal Principaler, options ...O) (A, error)
 }
 
 // ExchangeSchemer exchanges an artifact of type SA into an artifact of type TA
